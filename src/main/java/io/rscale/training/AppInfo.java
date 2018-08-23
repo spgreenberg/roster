16183b3ea408f88aa5a4527b732bda464941169e
Nick Martin <nicholas.martin@cgi.com>
2018-08-23 11:32:37 -0600

    roster config changes

--- src/main/java/io/rscale/training/AppInfo.java
+++ src/main/java/io/rscale/training/AppInfo.java
@@ -1,25 +1,5 @@
 package io.rscale.training;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class DisableBasicAuthAdapter extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-         http.csrf().disable()
-        	.authorizeRequests()
-        		.anyRequest().permitAll()
-        	.and().httpBasic().disable();
-    }
-
-}
-
 import java.util.List;
 
 public class AppInfo {
