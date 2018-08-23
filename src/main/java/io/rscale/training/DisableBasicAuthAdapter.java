4a52ce00db16c1f16c62f3fae9fea809865e3814
Steve Greenberg <spgreenberg@gmail.com>
2018-08-23 11:27:43 -0600

    svc discovery

--- src/main/java/io/rscale/training/DisableBasicAuthAdapter.java
+++ src/main/java/io/rscale/training/DisableBasicAuthAdapter.java
@@ -1,7 +1,6 @@
 package io.rscale.training;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
